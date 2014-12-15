db3c497dc9838c58747ffeef771dbe802d9cc3b5
DevOps-ipsum <coleman@ten7.com>
2014-12-14 22:35:47 -0600

    Buncha images, started about page w/parallax stuff

--- js/js.js
+++ js/js.js
@@ -4,11 +4,8 @@
 			$(".button-collapse").sideNav();
 			$('.materialboxed').materialbox();
 			$('ul.tabs').tabs();
+			$('.parallax').parallax();
+			
+			});
 
-			$('a[href*=#]').click(function(event){
-				event.preventDefault();
-				$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
-					});
-
-			});	
 }(jQuery));
